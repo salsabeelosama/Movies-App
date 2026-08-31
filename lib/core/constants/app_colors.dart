93c7a738ec5c4e2dbb3f3cad3cb634c3c59a2657
salsabeelosama <salsabeelosama217@gmail.com>
2026-08-31 00:38:40 -0700

    Initial commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const backgroundColor = Color(0xff121312);
+  static const mainColor =Color(0xff1F6BD00);
+  static const subColor = Color(0xff282A28);
+  static const whiteColor = Color(0xffFFFFFF);
+  static const redButtonColor = Color(0xffE82626);
+}
